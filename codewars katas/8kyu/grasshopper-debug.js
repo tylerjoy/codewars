47f1c2c2b99ddfc691344b2b6d8a11554c741d42
Tyler Joy <mrjoy206@gmail.com>
2023-10-21 16:44:03 -0700

    daily push

--- codewars katas/8kyu/grasshopper-debug.js
+++ codewars katas/8kyu/grasshopper-debug.js
@@ -1,3 +1,12 @@
-var a = "code";
-var b = "wa.rs";
-var name = a + b;
+function weatherInfo (temp) {
+  var c = convertToCelsius(temp);
+  if (c <= 0)
+    return (c + " is freezing temperature");
+  else
+    return (c + " is above freezing temperature");
+}
+
+function convertToCelsius (temperature) {
+  var celsius = (temperature - 32) * (5/9);
+  return celsius;
+}
