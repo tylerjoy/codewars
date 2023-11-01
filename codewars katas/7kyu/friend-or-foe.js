b004c26fd1112c418936e16b2a7318c151376bc6
Tyler Joy <mrjoy206@gmail.com>
2023-11-01 11:37:25 -0700

    daily push

---
+++ codewars katas/7kyu/friend-or-foe.js
@@ -0,0 +1,9 @@
+//function
+// function friend(friends){
+//   const myFriends = friends
+//                     .filter((name) => name.length === 4) 
+//   return myFriends
+// }
+
+//single line
+const friend = array => array.filter((name)=> name.length === 4)
