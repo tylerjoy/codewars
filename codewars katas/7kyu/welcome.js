03ed6c2b04b7616076755d44c70239a2eaf143ae
Tyler Joy <mrjoy206@gmail.com>
2023-11-02 09:50:01 -0700

    daily push

---
+++ codewars katas/7kyu/welcome.js
@@ -0,0 +1,23 @@
+function greet(language) {
+  return greetings[language] || greetings.english
+}
+
+const greetings = {
+  english: "Welcome",
+  czech: "Vitejte",
+  danish: "Velkomst",
+  dutch: "Welkom",
+  estonian: "Tere tulemast",
+  finnish: "Tervetuloa",
+  flemish: "Welgekomen",
+  french: "Bienvenue",
+  german: "Willkommen",
+  irish: "Failte",
+  italian: "Benvenuto",
+  latvian: "Gaidits",
+  lithuanian: "Laukiamas",
+  polish: "Witamy",
+  spanish: "Bienvenido",
+  swedish: "Valkommen",
+  welsh: "Croeso"
+};
