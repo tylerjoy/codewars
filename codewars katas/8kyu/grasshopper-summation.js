1ea4eab08eaf02670cf88bd98bd182169dd22b33
Tyler Joy <mrjoy206@gmail.com>
2023-11-10 11:12:31 -0800

    daily push

---
+++ codewars katas/8kyu/grasshopper-summation.js
@@ -0,0 +1,10 @@
+// var summation = function (num) {
+//   let sum = 0
+//   for(i=0;i<=num;i++){
+//     sum += i
+//   }
+//   return sum
+// }
+
+//single line
+const summation = n => n * (n + 1) / 2;
