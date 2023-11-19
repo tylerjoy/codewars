313fd2fde9d87524a25997382060e8023962e799
Tyler Joy <mrjoy206@gmail.com>
2023-11-19 10:18:14 -0800

    daily push

---
+++ codewars katas/7kyu/string-scramble.js
@@ -0,0 +1,6 @@
+// function scramble(str, arr){
+//   return arr.map((_,i)=> str[arr.indexOf(i)]).join('')
+// }
+
+//single line
+const scramble = (str, arr)=> arr.map((_,i)=> str[arr.indexOf(i)]).join('')
