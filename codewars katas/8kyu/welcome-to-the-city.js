8bbf2824e263663fa3ef83d22dcb3953d6679149
Tyler Joy <mrjoy206@gmail.com>
2023-11-20 12:51:19 -0800

    daily push

---
+++ codewars katas/8kyu/welcome-to-the-city.js
@@ -0,0 +1,6 @@
+// function sayHello( name, city, state ) {
+//   return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+// }
+
+//single line
+const sayHello = (name, city, state)=> `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
