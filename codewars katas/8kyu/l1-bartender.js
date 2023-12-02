6872a2bf299068f5684e6f5674d0c122174f6abd
Tyler Joy <mrjoy206@gmail.com>
2023-12-02 14:19:23 -0800

    daily push

--- codewars katas/8kyu/l1-bartender.js
+++ codewars katas/8kyu/l1-bartender.js
@@ -1,27 +1,3 @@
-// function getDrinkByProfession(param){
-//   param = param.toLowerCase()
-//   let output = ""
-
-//   if(param === 'jabroni') {
-//     output = 'Patron Tequila'
-//   }else if (param === 'school counselor'){
-//     output = 'Anything with Alcohol'
-//   }else if (param === 'programmer'){
-//     output = 'Hipster Craft Beer'
-//   }else if (param === 'bike gang member'){
-//     output = 'Moonshine'
-//   }else if (param === 'politician'){
-//     output = 'Your tax dollars'
-//   }else if (param === 'rapper'){
-//     output = 'Cristal'
-//   }else{
-//     output = 'Anything with Alcohol'
-//   }
-
-//   return output
-// }
-
-
 const drinks = {
   'jabroni' : 'Patron Tequila',
   'school counselor' : 'Anything with Alcohol',
