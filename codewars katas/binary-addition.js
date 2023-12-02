1a6cf5fd83ce104900f8ae58fc20774b8e5584ea
Tyler Joy <mrjoy206@gmail.com>
2023-12-02 13:25:40 -0800

    daily push

--- codewars katas/binary-addition.js
+++
@@ -1,9 +0,0 @@
-function addBinary(a,b) {
-  let sum = a + b
-  let reverseStr = ''
-  while (sum != 0){
-    reverseStr += sum % 2
-    sum = Math.floor(sum / 2)
-  }
-  return reverseStr.split('').reverse().join('')
-}
