8d34928861a3be166be75953de0b1b69986889e0
Tyler Joy <mrjoy206@gmail.com>
2023-12-28 12:37:54 -0800

    daily push

--- codewars katas/8kyu/stringy-strings.js
+++ codewars katas/8kyu/stringy-strings.js
@@ -1,12 +1,12 @@
 function stringy(size) {
-  let string = ""
-  
-      for(i=0;i<size;i++){
-        if(i % 2 === 0) {
-          string += 1
-        } else if(i % 2 === 1) {
-          string += 0
-        }
-      }
-      return string
+  let string = "";
+
+  for (i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+      string += 1;
+    } else if (i % 2 === 1) {
+      string += 0;
     }
+  }
+  return string;
+}
